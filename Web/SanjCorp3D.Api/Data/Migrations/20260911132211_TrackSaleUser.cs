b8d19b5ea85b0b60a5dc8babfa18851d5743767f
ricardo <richisanj@gmail.com>
2026-09-11 09:30:04 -0400

    Incluir pagina suprema y migracion de ventas

---
+++ Web/SanjCorp3D.Api/Data/Migrations/20260911132211_TrackSaleUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SanjCorp3D.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrackSaleUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
